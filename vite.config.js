bf85b0732f84c909c895d614063e83e680cc162f
yvanmadje-lab <yvanmadje@gmail.com>
2026-09-02 13:05:06 +0000

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Browncard Event',
+        short_name: 'Browncard Event',
+        description: 'Plateforme officielle de gestion et d’inscription aux événements Browncard',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
